ebbd746dd3132a88358fc576b7c21e0e5407f7f3
samwouters <73218012+samwouters@users.noreply.github.com>
2020-10-23 10:39:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function() {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('#up').on('click', function() {
+       $('html, body').animate( {
+           scrollTop: 0
+       });
+    });
+
+        AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
+});
